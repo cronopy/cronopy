967b0185cc8744c7f19799bffd31c9ed41cb0ae2
Carlos Andres Lopez <asrael@utp.edu.co>
2014-11-04 23:04:16 -0500

    Version: 1 desc: barra de herramientas

--- libcrono.py
+++ libcrono.py
@@ -1,39 +1,37 @@
 import wx
 
+Blanco=(0,0,0)
+
 class cronopy(wx.Frame):
   Pro_nom="ninguno"
   Tarea_nom="ninguna"
   def __init__(self, title, pos, size):
     wx.Frame.__init__(self, None, -1, title, pos, size)
-    m_proyecto = wx.Menu()
-    m_proyecto.Append(1, "Nuevo Proyecto")
-    m_proyecto.Append(2, "Abrir proyecto")
-    m_proyecto.Append(3, "Renombrar Proyecto")
-    m_proyecto.AppendSeparator()
-    m_proyecto.Append(4, "Exportar Proyecto")
-    m_proyecto.Append(5, "Importar Proyecto")  
 
-    m_tareas = wx.Menu()
-    m_tareas.Append(6, "Nueva Tarea")
-    m_tareas.Append(7, "Nueva Subtarea")
-    m_tareas.Append(8, "Renombrar Tarea")
-    m_tareas.Append(9, "Eliminar Tarea")
-
-    m_informacion= wx.Menu()
-    m_informacion.Append(10, "Acerca de...")
-    m_informacion.Append(11, "Salir")
-
-    menuBar = wx.MenuBar()
-    menuBar.Append(m_proyecto, "&Proyecto")
-    menuBar.Append(m_tareas, "&Tareas")
-    menuBar.Append(m_informacion, "&Ayuda")
-
-    self.SetMenuBar(menuBar)
+    
     self.CreateStatusBar()
+    self.crearToolbar()
     self.SetStatusText("Bienvenido a Cronopy!")
-    self.Bind(wx.EVT_MENU, self.SiInfo, id=10)
-    self.Bind(wx.EVT_MENU, self.SiSalir, id=11)
-    self.Bind(wx.EVT_MENU, self.SiPronuevo, id=1)
+
+
+
+  def crearToolbar(self):
+   toolbar= self.CreateToolBar();
+   for elemento in self.barraInicial():
+      self.crearEspacio(toolbar, *elemento)
+   toolbar.AddSeparator()
+   toolbar.Realize()
+
+  def crearEspacio(self, toolbar, etiqueta, archivo, ayuda, manejador):
+   if not etiqueta:
+     toolbar.AddSeparator()
+     return
+   icono=wx.Image(archivo, wx.BITMAP_TYPE_PNG).ConvertToBitmap()
+   espacio = toolbar.AddSimpleTool(-1, icono, etiqueta, ayuda)
+   self.Bind(wx.EVT_MENU, manejador, espacio)
+
+  def barraInicial(self):
+   return (("Proyectos", "imagenes/new.png", "Opciones de Proyecto", self.SiPronuevo), ("Tareas", "imagenes/open.png", "Opciones de tareas", self.SiInfo), ("Acerca de", "imagenes/save.png", "Informacion del aplicactivo", self.SiSalir))
 
   def SiSalir(self, event):
     self.Close()
@@ -46,7 +44,6 @@ class cronopy(wx.Frame):
     valor=wx.TextEntryDialog(self, "Nombre del proyecto: ", "Crear proyecto", "pro", style=wx.OK|wx.CANCEL)
     if valor.ShowModal() == wx.ID_OK:
        self.pro_nom=valor.GetValue()
-       print "You entered: %s" % self.pro_nom      
+       print "Tu digitaste: %s" % self.pro_nom      
        self.SetStatusText(self.pro_nom + ": " + self.Tarea_nom)
     valor.Destroy()  
-
